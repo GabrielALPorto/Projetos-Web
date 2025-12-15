a2c748d0cf0bbf453ffaaa90260379d5edc6839f
Bielbr123 <gabriellyra46@gmail.com>
2025-12-15 09:57:01 -0300

    Chores: Preparar para merger com a main com o objetivo de organizar os arquivos e deixar a main atualizada. A SPA ainda não foi implementada, irei criar outra branch para isso

--- replante_a_floresta/assets/js/components/sidebar_redirect.js
+++
@@ -1,5 +0,0 @@
-import {conteudoIndex, conteudoDoacoes, conteudoProjetos, conteudoSobre} from '../pages_contents/contentsHTML.js';   
-import conteudoController from spa.js;
-
-const sobre = document.querySelector("#sobre");
-window.addEventListener("load", )
